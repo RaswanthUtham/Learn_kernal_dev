4069f936ec009efcbedfb2103b0ad40bed7650de
Raswanth <u.raswanth@gmail.com>
2024-06-15 00:41:54 +0530

    implemented  physical mddr and virtual addr mapping

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -94,6 +94,12 @@ void kernel_main()
    
     /* switch to kernel page directory */
     paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
+    
+    /* get ptr from heap. It is a physical address 0x1000000 */
+    char* ptr = kzalloc(4096);
+
+    /* map the physical addr 0x1000000 to 0x1000 */
+    paging_set(paging_4gb_chunk_get_directory(kernel_chunk), (void*)0x1000, (uint32_t)ptr | PAGING_ACCESS_FROM_ALL | PAGING_IS_PRESENT | PAGING_IS_WRITEABLE);
 
     /* Enable Paging */
     enable_paging();
@@ -137,18 +143,26 @@ void kernel_main()
      // }
 
     /* Malloc Test 3*/
-     void *ptr1 = kmalloc(5000);  // 000-1999 2
-     void *ptr2 = kmalloc(50);    //  2000 - 2999 1
-     kfree(ptr2);                 // 2000 - 2999 1
-     kfree(ptr1);                 // 000 - 1999 2
-     void *ptr3 = kmalloc(12000); // 000 - 2999 3
-     void *ptr4 = kmalloc(2);     // 3000 - 3999
-     void *ptr5 = kmalloc(5);     // 4000 - 4999
-     kfree(ptr3);                 // 0 - 2999
-     void *ptr6 = kmalloc(3900);  // 000 - 999
-     void *ptr7 = kmalloc(8000);  // 1000 - 2999
-     void *ptr8 = kmalloc(199);   // 5000 - 5999
-     if (ptr1 || ptr2 || ptr3 || ptr4 || ptr5 || ptr6 || ptr7 || ptr8)
-     {
-     }
+    //  void *ptr1 = kmalloc(5000);  // 000-1999 2
+    //  void *ptr2 = kmalloc(50);    //  2000 - 2999 1
+    //  kfree(ptr2);                 // 2000 - 2999 1
+    //  kfree(ptr1);                 // 000 - 1999 2
+    //  void *ptr3 = kmalloc(12000); // 000 - 2999 3
+    //  void *ptr4 = kmalloc(2);     // 3000 - 3999
+    //  void *ptr5 = kmalloc(5);     // 4000 - 4999
+    //  kfree(ptr3);                 // 0 - 2999
+    //  void *ptr6 = kmalloc(3900);  // 000 - 999
+    //  void *ptr7 = kmalloc(8000);  // 1000 - 2999
+    //  void *ptr8 = kmalloc(199);   // 5000 - 5999
+    //  if (ptr1 || ptr2 || ptr3 || ptr4 || ptr5 || ptr6 || ptr7 || ptr8)
+    //  {
+    //  }
+    
+    /* paging test */
+    char *ptr2 = (char *) 0x1000;
+    ptr2[0] = 'Z';
+    ptr2[1] = 'p';
+
+    print(ptr2);
+    print(ptr);
 }
--- kernel/src/memory/paging/paging.c
+++ kernel/src/memory/paging/paging.c
@@ -1,4 +1,5 @@
 #include "paging.h"
+#include "status.h"
 #include "memory/heap/kheap.h"
 
 void paging_load_directory(uint32_t* directory); /* asm function */
@@ -34,4 +35,56 @@ void paging_switch(uint32_t* directory)
 uint32_t* paging_4gb_chunk_get_directory(struct paging_4gb_chunk* chunk)
 {
     return chunk->directory_entry;
+}
+
+bool paging_is_aligned(void* addr)
+{
+    return ((uint32_t)addr % PAGING_PAGE_SIZE) == 0;
+}
+
+int paging_get_indexes(void* virtual_address, uint32_t* directory_index_out, uint32_t* table_index_out)
+{
+    int res = 0;
+    if (!paging_is_aligned(virtual_address))
+    {
+        res = -EINVARG;
+        goto out;
+    }
+    
+    /* directory index = virtual address / page table size */
+    *directory_index_out = ((uint32_t)virtual_address / (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE));
+
+    /* table index = virtual address % page table size / page size */
+    *table_index_out = ((uint32_t) virtual_address % (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE) / PAGING_PAGE_SIZE);
+out:
+    return res;
+}
+
+int paging_set(uint32_t* directory, void* virt, uint32_t val)
+{
+    /* map absolute address (val) to virtual address (virt) */
+
+    if (!paging_is_aligned(virt))
+    {
+        return -EINVARG;
+    }
+
+    uint32_t directory_index = 0;
+    uint32_t table_index = 0;
+    int res = paging_get_indexes(virt, &directory_index, &table_index);
+    if (res < 0)
+    {
+        return res;
+    }
+    
+    /* Retrieves the directory entry from the page directory using the directory_index */
+    uint32_t entry = directory[directory_index];
+
+    /* Calculates the base address of the page table by masking the lower 12 bits of the entry to zero (ensuring it is aligned to a page boundary) */
+    uint32_t* table = (uint32_t*)(entry & 0xfffff000);
+
+    /* Map the absolute address (val) to the page */
+    table[table_index] = val;
+
+    return 0;
 }
