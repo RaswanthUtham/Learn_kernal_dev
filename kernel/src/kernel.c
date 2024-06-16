00dc8ad1549e5b65f91353da70527a51eb037525
Raswanth <u.raswanth@gmail.com>
2024-06-16 14:49:10 +0530

    Implemented disk streamer

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -159,11 +159,17 @@ void kernel_main()
    //  print(ptr);
    
     /* pathparser test */
-    struct path_root* root_path = pathparser_parse("0:/bin/shell.exe", NULL);
+    // struct path_root* root_path = pathparser_parse("0:/bin/shell.exe", NULL);
 
-    if(root_path)
-    {
+    // if(root_path)
+    // {
 
 
-    }
+    // }
+
+    struct disk_stream* stream = diskstreamer_new(0);
+    diskstreamer_seek(stream, 0x201);
+    unsigned char c = 0;
+    diskstreamer_read(stream, &c, 1);
+    while(1) {}
 }
