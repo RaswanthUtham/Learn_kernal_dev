1a438a44352f7e95ffc3d0bc6dd1caf27d7b1489
Raswanth <u.raswanth@gmail.com>
2024-06-28 03:04:46 +0530

    Implemented VFS open function

--- kernel/src/fs/file.c
+++ kernel/src/fs/file.c
@@ -2,6 +2,8 @@
 #include "config.h"
 #include "memory/memory.h"
 #include "memory/heap/kheap.h"
+#include "string/string.h"
+#include "disk/disk.h"
 #include "fat/fat16.h"
 #include "status.h"
 #include "kernel.h"
@@ -115,7 +117,94 @@ struct filesystem* fs_resolve(struct disk* disk)
     return fs;
 }
 
-int fopen(const char* filename, const char* mode)
+FILE_MODE file_get_mode_by_string(const char* str)
 {
-    return -EIO;
+    FILE_MODE mode = FILE_MODE_INVALID;
+    if (strncmp(str, "r", 1) == 0)
+    {
+        mode = FILE_MODE_READ;
+    }
+    else if(strncmp(str, "w", 1) == 0)
+    {
+        mode = FILE_MODE_WRITE;
+    }
+    else if(strncmp(str, "a", 1) == 0)
+    {
+        mode = FILE_MODE_APPEND;
+    }
+    return mode;
+}
+
+/* This function opens a file given its filename and mode, and returns a file descriptor index. 
+ * const char* filename: A string representing the path to the file.
+ * const char* mode_str: A string representing the mode in which to open the file (e.g., "r", "w").
+ */
+int fopen(const char* filename, const char* mode_str)
+{
+    int res = 0;
+    struct path_root* root_path = pathparser_parse(filename, NULL);
+    if (!root_path)
+    {
+        res = -EINVARG;
+        goto out;
+    }
+
+    /* We cannot have just a root path 0:/ 0:/test.txt */
+    if (!root_path->first)
+    {
+        res = -EINVARG;
+        goto out;
+    }
+
+    /* Ensure the disk we are reading from exists */
+    struct disk* disk = disk_get(root_path->drive_no);
+    if (!disk)
+    {
+        res = -EIO;
+        goto out;
+    }
+    
+    /* Ensure Disk Has a Filesystem */
+    if (!disk->filesystem)
+    {
+        res = -EIO;
+        goto out;
+    }
+    
+    /* Validate File Mode */
+    FILE_MODE mode = file_get_mode_by_string(mode_str);
+    if (mode == FILE_MODE_INVALID)
+    {
+        res = -EINVARG;
+        goto out;
+    }
+    
+    /* Open the File in the Filesystem */
+    void* descriptor_private_data = disk->filesystem->open(disk, root_path->first, mode);
+    if (ISERR(descriptor_private_data))
+    {
+        res = ERROR_I(descriptor_private_data);
+        goto out;
+    }
+
+    /* Allocate a New File Descriptor */
+    struct file_descriptor* desc = 0;
+    res = file_new_descriptor(&desc);
+    if (res < 0)
+    {
+        goto out;
+    }
+
+    /* Set Up the File Descriptor */
+    desc->filesystem = disk->filesystem;
+    desc->private = descriptor_private_data;
+    desc->disk = disk;
+    res = desc->index;
+
+out:
+    // fopen shouldnt return negative values
+    if (res < 0)
+        res = 0;
+
+    return res;
 }
