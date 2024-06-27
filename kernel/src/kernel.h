b666f9a91eb0b697ce578dc70cdef656d5f5b2de
Raswanth <u.raswanth@gmail.com>
2024-06-28 03:04:46 +0530

    Implemented VFS open function

--- kernel/src/kernel.h
+++ kernel/src/kernel.h
@@ -13,6 +13,10 @@
 /* Register Adresses */
 #define VIDEO_ADDRESS 0xB8000
 
+/* macro functions */
+#define ERROR(value) (void*)(value)
+#define ERROR_I(value) (int)(value)
+#define ISERR(value) ((int)value < 0)
 
 /* Structures */
 
